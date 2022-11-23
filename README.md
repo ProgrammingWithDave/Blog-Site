062a59054e29fc8c8068c68a521102fb697f35b3
ProgrammingWithDave <davekleinex@gmail.com>
2022-11-23 14:35:26 -0800

    Source Files

---
+++ README.md
@@ -0,0 +1,2 @@
+# Blog Site
+ Just A Blog Site That I'm Going To Be Working On With HTML, CSS, And JavaScript.
---
+++ script.js
@@ -0,0 +1,5 @@
+function msg() {
+    console.log("Hello, I am the creator of this project. This project is not yet finished, but you can contribute to the project on Github. If you have any further questions, don't hesitate to contact me at davekleinex@gmail.com")
+}
+
+msg();
